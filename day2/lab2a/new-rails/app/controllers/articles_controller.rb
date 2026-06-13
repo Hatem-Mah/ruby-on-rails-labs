8b8adf42fc3ebde67cf9ffd7c2a98d50275f48c4
Hatem <hatem@example.com>
2026-06-13 03:09:19 +0300

    finish day2 lab fixes

--- day2/lab2a/new-rails/app/controllers/articles_controller.rb
+++ day2/lab2a/new-rails/app/controllers/articles_controller.rb
@@ -11,13 +11,32 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
